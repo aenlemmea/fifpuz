f576cd5cc9cd5c67ae3d30c149431f23751a24d5
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-10-19 12:18:04 +0000

    Done for now. Will fix some bugs soon:tm:

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ auto main(int argc, char** argv) -> int {
 	bool isRunning = false;
 	Controller cntrl(Board{});
  
-
+	// BUG: If I try to force "pull" the 13 value of the solved_board to empty tile. Weird thing is copied. Fix this
 	std::cout << "Use [WASD] to move, [q] to quit\n";
 	while(!isRunning) {
 		std::cout << "Enter > ";
